1d4e5feda0d83297ec5ecd8cb58ca879a3cd0d30
Swayam Kuckreja <swayam.kuckreja@gmail.com>
2025-12-19 02:15:36 +0100

    renamed some files and organised repo into folders

---
+++ julia/tests/compare_tut_incomp_w_temp.jl
@@ -0,0 +1,7 @@
+# this code compares sol_tutorial with sol_incomp_w_temp_finished
+# to see if the velocity and pressure stuff is a perfect match.
+
+# this acts as a unit/ system test for incomp_w_temp.
+
+
+
